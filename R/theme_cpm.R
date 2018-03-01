c8dfb94a5bec22f1fabb6466466ace4a7ac0f627
Rico Derks <r.j.e.derks@lumc.nl>
2018-03-01 08:20:23 +0100

    set color for stript text

--- R/theme_cpm.R
+++ R/theme_cpm.R
@@ -1,6 +1,8 @@
 #' @title Set the cpm theme for ggplot2
 #'
-#' @description Set the cpm theme for ggplot2 at the moment this is only the minimal theme (theme_minimal)
+#' @description Set the cpm theme for ggplot2.
+#'
+#' @details Set the CPM theme for ggplot2. The theme is based on theme_minimal()
 #'
 #' @export
 #' @importFrom ggplot2 theme_minimal theme element_rect
@@ -12,7 +14,8 @@ theme_cpm <- function() {
 
   my_theme <- my_theme +
     ggplot2::theme(panel.background = ggplot2::element_rect(fill = "#cce5f3", colour = "white"),
-                   strip.background = ggplot2::element_rect(fill = "#007cc2", colour = "white"))
+                   strip.background = ggplot2::element_rect(fill = "#007cc2", colour = "white"),
+                   strip.text = ggplot2::element_text(colour = "white"))
 
   return(my_theme)
 }
